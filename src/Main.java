fe2db21a0e4691069ae828e06333732ba7bd9c02
AlexGekko88 <115148133+AlexGekko88@users.noreply.github.com>
2022-12-27 10:55:26 +0300

    комит

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        int[] sales = {500, 1000, 560, 12000, 2500, 3450, 30120, 8900};
+        SalesManager salesManager = new SalesManager(sales);
+        System.out.println("Самая крупная  продажа на " + salesManager.max() + " руб.");
     }
 }
